73e3db41d37e2dc697f7ed88456e160b6feaecd2
silver-it <silver.19920220@gmail.com>
2020-07-09 17:59:38 -0700

    feat: integrated with backend

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
+ARG port
+
 # Stage 1 - the build process
-FROM node:12.16 as build-deps
+FROM node:12.16 as build-react
 WORKDIR /usr/src/app
 COPY package.json package-lock.json ./
 # RUN yarn
 COPY . ./
+RUN npm install
 RUN npm run build
 
 # Stage 2 - the production environment
 FROM nginx:1.19
-COPY --from=build-deps /usr/src/app/build /usr/share/nginx/html
-EXPOSE 80
+COPY --from=build-react /usr/src/app/build /usr/share/nginx/html
+EXPOSE $port
 CMD ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const getUsers = () => {
+    axios.get('http://localhost:3030/users').then(res => {
+      const { data } = res;
+      if (typeof data === 'number') {
+        setCount(data);
+      } else {
+        setCount(0);
+      }
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {count} Users Found.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="App-link" onClick={getUsers}>
+          Get Users Count
+        </button>
       </header>
     </div>
   );
